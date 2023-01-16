0519bc0f24eab6222a2ae70dcb5bf93468c5a372
julschoen <julianschoen@gmx.de>
2023-01-16 15:44:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
     # General Training
-    parser.add_argument('--batch-size', type=int, default=128, metavar='N', help='input batch size for training (default: 64)')
+    parser.add_argument('--batch-size', type=int, default= 100, metavar='N', help='input batch size for training (default: 64)')
     parser.add_argument('--niter', type=int, default=20000, metavar='N', help='number of epochs to train (default: 14)')
-    parser.add_argument('--lr', type=float, default=1e-3, metavar='LR', help='learning rate (default: 0.001)')
-    parser.add_argument('--lrIms', type=float, default=1e-1, metavar='LR', help='learning rate (default: 0.001)')
+    parser.add_argument('--lr', type=float, default=1e-5, metavar='LR', help='learning rate (default: 0.001)')
+    parser.add_argument('--lrIms', type=float, default=1e-3, metavar='LR', help='learning rate (default: 0.001)')
     parser.add_argument('--num_ims', type=int, default=10)
     parser.add_argument('--cifar', type=bool, default=False)
     parser.add_argument('--cmmd', type=bool, default=False)
@@ -27,7 +27,7 @@ def main():
         transforms.ToTensor()
         ])
     if args.cifar:
-        dataset1 = datasets.CIFAR('../data/', train=True, download=True,
+        dataset1 = datasets.CIFAR10('../data/', train=True, download=True,
                            transform=transform)
     else:
         dataset1 = datasets.MNIST('../data/', train=True, download=True,
