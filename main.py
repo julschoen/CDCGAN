e3d1dba6c0953a98f10c43a2e810f52e4ab3322a
julschoen <julianschoen@gmx.de>
2023-01-17 11:09:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
     parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--init_ims', type=bool, default=False)
     parser.add_argument('--log_dir', type=str, default='./log')
+    parser.add_argument('--cont', type=bool, default=False)
 
     # Model Params
     parser.add_argument('--k', type=int, default=100)
