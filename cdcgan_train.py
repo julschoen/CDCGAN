069dc4f215d71f0e0d70e0f6d2678334fa66c761
julschoen <julianschoen@gmx.de>
2023-01-20 11:26:51 +0100

    Update cdcgan_train.py

--- cdcgan_train.py
+++ cdcgan_train.py
@@ -195,7 +195,7 @@ class Trainer():
             else:
                 self.losses.append((errD.item(), errG.item()))
 
-            if self.p.reloadD and t > 0 and (t%500) == 0:
+            if self.p.reloadD and t > 0 and (t%2000) == 0:
                 if self.p.biggan and self.p.cifar:
                     self.model = BigGAN().to(self.p.device)
                 else:
