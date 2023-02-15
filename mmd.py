436c4c79f75b6b8fe531b2a82cd753e63d0f1232
julschoen <julianschoen@gmx.de>
2023-02-15 11:22:53 +0100

    Update mmd.py

--- mmd.py
+++ mmd.py
@@ -111,6 +111,11 @@ def _mmd2(K_XX, K_XY, K_YY, rep=False, const_diagonal=False, biased=False):
             + Kt_YY_sum / (m * (m - 1))
             - 2.0 * K_XY_sum / (m * m))
 
+    if rep:
+        mmd2 = ((Kt_XX_sum + sum_diag_X)
+            - (Kt_YY_sum + sum_diag_Y))
+
+
     return mmd2
 
 
