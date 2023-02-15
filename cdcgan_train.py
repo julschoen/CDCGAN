1962de69a9961b5582228f3a75996c67dba630b9
julschoen <julianschoen@gmx.de>
2023-02-15 11:47:37 +0100

    Update cdcgan_train.py

--- cdcgan_train.py
+++ cdcgan_train.py
@@ -173,11 +173,9 @@ class Trainer():
 
                     mmd2_D = mix_rbf_mmd2(encX, encY, self.sigma_list, rep=self.p.repulsion)
                     if self.p.repulsion:
-                        mmd2_D = F.relu(mmd2_D)
-                        errD = torch.sqrt(mmd2_D)
+                        errD = mmd2_D
                     else:
-                        mmd2_D = F.relu(mmd2_D)
-                        errD = -torch.sqrt(mmd2_D)
+                        errD = -mmd2_D
                 errD.backward()
                 self.optD.step()
 
